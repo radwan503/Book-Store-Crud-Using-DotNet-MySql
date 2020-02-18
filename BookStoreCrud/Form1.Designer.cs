adec6ae4b07d1889f1d112feed948862abb25032
radwan.ahmed@tigerit.com <radwan.ahmed@tigerit.com>
2020-02-18 15:46:25 +0600

    Simple Csharp crud project

---
+++ BookStoreCrud/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace BookStoreCrud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtBookName = new System.Windows.Forms.TextBox();
+            this.dgvBook = new System.Windows.Forms.DataGridView();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBook)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Book Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(28, 282);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtBookName
+            // 
+            this.txtBookName.Location = new System.Drawing.Point(94, 65);
+            this.txtBookName.Name = "txtBookName";
+            this.txtBookName.Size = new System.Drawing.Size(215, 20);
+            this.txtBookName.TabIndex = 2;
+            this.txtBookName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dgvBook
+            // 
+            this.dgvBook.AllowUserToAddRows = false;
+            this.dgvBook.AllowUserToDeleteRows = false;
+            this.dgvBook.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBook.Location = new System.Drawing.Point(345, 65);
+            this.dgvBook.Name = "dgvBook";
+            this.dgvBook.ReadOnly = true;
+            this.dgvBook.Size = new System.Drawing.Size(408, 240);
+            this.dgvBook.TabIndex = 3;
+            this.dgvBook.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvBook_CellContentClick);
+            this.dgvBook.DoubleClick += new System.EventHandler(this.dgvBook_DoubleClick);
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(94, 108);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(215, 20);
+            this.txtAuthor.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Author Name";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(94, 150);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(215, 114);
+            this.txtDescription.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Description";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(130, 282);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(234, 282);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(676, 34);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(77, 23);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(345, 36);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(325, 20);
+            this.txtSearch.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 359);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvBook);
+            this.Controls.Add(this.txtBookName);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Book Store Crud App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBook)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtBookName;
+        private System.Windows.Forms.DataGridView dgvBook;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+    }
+}
+
---
+++ BookStoreCrud/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace BookStoreCrud
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Server=localhost;Database=book_store;Uid=root;Pwd=root;";
+
+        int bookID = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Clear();
+            GridFill();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mySqlCon = new MySqlConnection(connectionString))
+            {
+                mySqlCon.Open();
+                MySqlCommand mySqlCommand = new MySqlCommand("BookAddOrEdit", mySqlCon);
+                mySqlCommand.CommandType = CommandType.StoredProcedure;
+                mySqlCommand.Parameters.AddWithValue("_BookID", bookID);
+                mySqlCommand.Parameters.AddWithValue("_BookName", txtBookName.Text.Trim());
+                mySqlCommand.Parameters.AddWithValue("_Author", txtAuthor.Text.Trim());
+                mySqlCommand.Parameters.AddWithValue("_Description", txtDescription.Text.Trim());
+                mySqlCommand.ExecuteNonQuery();
+                MessageBox.Show("Submitted Successfully");
+                Clear();
+                GridFill();
+           
+            }
+
+        }
+
+        void GridFill()
+        {
+            using (MySqlConnection mySqlCon = new MySqlConnection(connectionString))
+            {
+                mySqlCon.Open();
+
+                MySqlDataAdapter sqlData = new MySqlDataAdapter("BookViewAll", mySqlCon);
+                sqlData.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataTable dataTableBook = new DataTable();
+                sqlData.Fill(dataTableBook);
+                dgvBook.DataSource = dataTableBook;
+                dgvBook.Columns[0].Visible = false;
+            }
+        }
+
+
+        void Clear()
+        {
+            txtBookName.Text = txtAuthor.Text = txtDescription.Text = txtSearch.Text = "";
+            bookID = 0;
+            btnSave.Text = "save";
+            btnDelete.Enabled = false;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            using(MySqlConnection mySqlConn = new MySqlConnection(connectionString))
+            {
+                mySqlConn.Open();
+                MySqlCommand sqlCommand = new MySqlCommand("BookDeleteById", mySqlConn);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddWithValue("_BookID", bookID);
+                sqlCommand.ExecuteNonQuery();
+                MessageBox.Show("Delete Successfully");
+                Clear();
+                GridFill();
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void dgvBook_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dgvBook_DoubleClick(object sender, EventArgs e)
+        {
+            if(dgvBook.CurrentRow.Index != -1)
+            {
+                txtBookName.Text = dgvBook.CurrentRow.Cells[1].Value.ToString();
+                txtAuthor.Text = dgvBook.CurrentRow.Cells[2].Value.ToString();
+                txtDescription.Text = dgvBook.CurrentRow.Cells[3].Value.ToString();
+                bookID = Convert.ToInt32(dgvBook.CurrentRow.Cells[0].Value.ToString());
+                btnSave.Text = "Update";
+                btnDelete.Enabled = Enabled;
+            }
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mySqlConn = new MySqlConnection(connectionString))
+            {
+                mySqlConn.Open();
+
+                MySqlDataAdapter sqlData = new MySqlDataAdapter("BookSearchByValue", mySqlConn);
+                sqlData.SelectCommand.CommandType = CommandType.StoredProcedure;
+                sqlData.SelectCommand.Parameters.AddWithValue("_SearchValue", txtSearch.Text);
+                DataTable dataTableBook = new DataTable();
+                sqlData.Fill(dataTableBook);
+                dgvBook.DataSource = dataTableBook;
+                dgvBook.Columns[0].Visible = false;
+
+            }
+        }
+    }
+}
